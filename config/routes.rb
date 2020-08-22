0d19217e50c055a710bbd7ddd4774b084285a74b
Renato Rosa Franco <renato_ny@live.com>
2020-08-22 11:40:23 -0300

    Interactor added to the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   
   # Application
   root to: 'home#index'
   post 'persons' => 'persons#create', as: :people
+
+  # Sidekiq
+  mount Sidekiq::Web => '/sidekiq'
 end
